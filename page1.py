7ee198d5f74456f2a7ec77774a0f6e703ed4ca0e
Denis Silin <Silin.dns@gmail.com>
2023-03-18 17:10:58 +0300

    remove some files

--- page1.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def page1():
-    st.title('Page 1')
-    st.write('Welcome to Page 1')
--- page2.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def page2():
-    st.title('Page 2')
-    st.write('Welcome to Page 2')
